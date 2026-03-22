66b51946e6dc78432703c9a40d752ca8fcff8641
krushnathombare <krushnathombare@samanyastra.com>
2026-03-22 17:18:21 +0530

    fix path import error

--- portfolio_builder/urls.py
+++ portfolio_builder/urls.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.contrib import admin
+from django.urls import path, include
 
 def home(request):
     return HttpResponse("🚀 Portfolio Backend Running")
