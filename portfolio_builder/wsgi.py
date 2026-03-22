e07766661341c9bb8638bec2c0c17e1326173fd8
krushnathombare <krushnathombare@samanyastra.com>
2026-03-22 17:24:59 +0530

    create superuser

--- portfolio_builder/wsgi.py
+++ portfolio_builder/wsgi.py
@@ -1,5 +1,21 @@
-import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_builder.settings')
 application = get_wsgi_application()
+
+
+import os
+import django
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_builder.settings')
+# django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(email="admin@gmail.com").exists():
+    User.objects.create_superuser(
+        email="thombarek362@gmail.com",
+        password="admin123"
+    )
