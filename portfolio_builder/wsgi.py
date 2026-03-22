e5027e89e085b6bbacd1799bd6bda0513d0651b3
krushnathombare <krushnathombare@samanyastra.com>
2026-03-22 17:33:28 +0530

    fix wsgi application error

--- portfolio_builder/wsgi.py
+++ portfolio_builder/wsgi.py
@@ -1,9 +1,9 @@
 import os
-import django
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_builder.settings')
-django.setup()
 
+application = get_wsgi_application()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
